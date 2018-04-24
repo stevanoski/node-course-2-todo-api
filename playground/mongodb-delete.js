730b5329b0ab8147411fd9747a151ddae3a702c7
stevanoski <aleks_stevanoski@hotmail.com>
2018-04-24 19:00:23 +0200

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,41 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    const db = client.db('TodoApp');
+
+    //deleteMany
+    // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result)=>{
+    //     console.log(result);
+    // });
+
+    //deleteOne
+    // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result)=>{
+    //     console.log(result);
+    // });
+
+
+    //findOneAndDelete
+    // db.collection('Todos').findOneAndDelete({completed: false}).then((result)=>{
+    //     console.log(result);
+    // });
+
+    //deleteMany from Users
+    // db.collection('Users').deleteMany({name: 'Paul'}).then((result)=>{
+    //         console.log(result);
+    //     });
+
+    //findOneAndDelete from Users
+    db.collection('Users').findOneAndDelete({_id: new ObjectID('5adf60017717034eb86a8a9a')}).then((result)=>{
+            console.log(result);
+        });
+
+    // client.close();
+});
