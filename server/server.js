93c950cc7d86f1c1b7dac96446484f832100c890
stevanoski <aleks_stevanoski@hotmail.com>
2018-04-25 19:30:21 +0200

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,8 +21,18 @@ app.post('/todos', (req, res) => {
         res.status(400).send(e);
     });
 });
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos });
+
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
 app.listen(3000, () => {
     console.log('Started port 3000');
 });
 
-module.exports = {app};
+module.exports = { app };
